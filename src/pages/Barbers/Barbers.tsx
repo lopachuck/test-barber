2b4e5f5ffa998b4a393d067e6abb9fee80fb26c8
Alex L <o.v.lopachuk@gmail.com>
2023-12-30 01:27:45 +0200

    rm mail to config

--- src/pages/Barbers/Barbers.tsx
+++ src/pages/Barbers/Barbers.tsx
@@ -2,6 +2,7 @@ import {FC} from 'react';
 import { PageWrapper } from "../Contacts/contacts.styles";
 import {t} from "../../utils/translate/t";
 import * as S from "./barbers.styles"
+import {BARBERSHOP_MAIL_ADDRESS} from "../../config/config";
 
 const Barbers: FC = () => {
     return (
@@ -14,9 +15,9 @@ const Barbers: FC = () => {
                 <S.BarbersInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
-                            <source srcSet={'/img/barbers/Valeria.webp'} type="image/webp"/>
-                            <source srcSet={'/img/barbers/Valeria.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Valeria.webp'} alt=""/>
+                            <source srcSet={'/img/barbers/Valeria.webp'} type={"image/webp"}/>
+                            <source srcSet={'/img/barbers/Valeria.jpg'} type={"image/jpg"}/>
+                            <img src={'/img/barbers/Valeria.jpg'} loading={'lazy'} alt=""/>
                         </S.BarberPhoto>
                         <S.BarberName>Valeria</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -25,7 +26,7 @@ const Barbers: FC = () => {
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Valerii.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Valerii.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Valerii.webp'} alt=""/>
+                            <img src={'/img/barbers/Valerii.jpg'} loading={'lazy'} alt=""/>
                         </S.BarberPhoto>
                         <S.BarberName>Valerii</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -34,7 +35,7 @@ const Barbers: FC = () => {
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Viktoria.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Viktoria.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Viktoria.webp'} alt=""/>
+                            <img src={'/img/barbers/Viktoria.jpg'} loading={'lazy'} alt=""/>
                         </S.BarberPhoto>
                         <S.BarberName>Viktoria</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -43,7 +44,7 @@ const Barbers: FC = () => {
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Alina.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Alina.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Valeria.webp'} alt=""/>
+                            <img src={'/img/barbers/Valeria.jpg'} loading={'lazy'} alt=""/>
                         </S.BarberPhoto>
                         <S.BarberName>Alina</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -52,7 +53,7 @@ const Barbers: FC = () => {
                 <S.JoinUsInfoWrapper>
                     <S.InfoTitle>{t('barbers_info_title')}</S.InfoTitle>
                     <S.InfoText>
-                        {t('barbers_info_text')} - <S.MailLink href={'mailto:unclevebarbershop@gmail.com'}>unclevebarbershop@gmail.com</S.MailLink>
+                        {t('barbers_info_text')} - <S.MailLink href={`mailto:${BARBERSHOP_MAIL_ADDRESS}`}>{BARBERSHOP_MAIL_ADDRESS}</S.MailLink>
                     </S.InfoText>
                 </S.JoinUsInfoWrapper>
             </PageWrapper>
