c3ec541bc976c8c9ecacbf325e3e4fdc6f01ee12
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2023-12-30 08:28:59 +0200

    fix alt Barbers images. fix links

--- src/layouts/Footer/footer.tsx
+++ src/layouts/Footer/footer.tsx
@@ -1,11 +1,11 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import * as S from './footer.styles'
 import {
     BARBERSHOP_FACEBOOK_LINK,
     BARBERSHOP_INSTAGRAM_LINK,
     BARBERSHOP_MAIL_ADDRESS,
-    BARBERSHOP_PHONE
-} from "../../config/config";
+    BARBERSHOP_PHONE,
+} from '../../config/config'
 
 const Footer: FC = () => {
     return (
@@ -16,26 +16,27 @@ const Footer: FC = () => {
                         <S.InfoTitle>Stay Up to Date</S.InfoTitle>
                         <span>Subcscribe to our social media</span>
                         <S.SocialIconWrapper>
-                            <S.SocialIcon to={BARBERSHOP_INSTAGRAM_LINK} target={'_blank'}>
-                                <img src={'/svg/icons/instagram.svg'} alt={'insta'}/>
+                            <S.SocialIcon to={BARBERSHOP_INSTAGRAM_LINK} target={'_blank'} rel="noopener noreferrer">
+                                <img src={'/svg/icons/instagram.svg'} alt={'instagram'} />
                             </S.SocialIcon>
-                            <S.SocialIcon to={BARBERSHOP_FACEBOOK_LINK} target={'_blank'}>
-                                <img src={'/svg/icons/facebook.svg'} alt={'insta'}/>
+                            <S.SocialIcon to={BARBERSHOP_FACEBOOK_LINK} target={'_blank'} rel="noopener noreferrer">
+                                <img src={'/svg/icons/facebook.svg'} alt={'facebook'} />
                             </S.SocialIcon>
                         </S.SocialIconWrapper>
                     </S.SocialWrapper>
-                    <S.Logo src={'/svg/icons/logo_big.svg'}/>
+                    <S.Logo src={'/svg/icons/logo_big.svg'} />
                     <S.ContactsWrapper>
                         <S.InfoTitle>Contacts</S.InfoTitle>
                         <S.ContactsDetails>
                             <S.Address>
-                                <a href={"https://waze.com/ul/hud9d5sh68"} target={'_blank'}>Fr.R.Kreutzwaldi 7, 10126 Tallinn</a>
+                                <a href={'https://waze.com/ul/hud9d5sh68'} target={'_blank'} rel="noopener noreferrer">
+                                    Fr.R.Kreutzwaldi 7, 10126 Tallinn</a>
                             </S.Address>
                             <S.Phone>
-                                <a href={'tel:+37258365820'}>+37258365820</a>
+                                <a href={`tel:${BARBERSHOP_PHONE}`}>${BARBERSHOP_PHONE}</a>
                             </S.Phone>
                             <S.Mail>
-                                <a href={`mailto:${BARBERSHOP_MAIL_ADDRESS}`}>unclevebarbershop@gmail.com</a>
+                                <a href={`mailto:${BARBERSHOP_MAIL_ADDRESS}`}>${BARBERSHOP_MAIL_ADDRESS}</a>
                             </S.Mail>
                         </S.ContactsDetails>
                     </S.ContactsWrapper>
--- src/pages/Barbers/Barbers.tsx
+++ src/pages/Barbers/Barbers.tsx
@@ -2,6 +2,7 @@ import {FC} from 'react';
 import { PageWrapper } from "../Contacts/contacts.styles";
 import {t} from "../../utils/translate/t";
 import * as S from "./barbers.styles"
+import { BARBERSHOP_MAIL_ADDRESS } from '../../config/config'
 
 const Barbers: FC = () => {
     return (
@@ -16,7 +17,7 @@ const Barbers: FC = () => {
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Valeria.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Valeria.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Valeria.webp'} alt=""/>
+                            <img src={'/img/barbers/Valeria.webp'} alt="Valeria"/>
                         </S.BarberPhoto>
                         <S.BarberName>Valeria</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -25,25 +26,25 @@ const Barbers: FC = () => {
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Valerii.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Valerii.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Valerii.webp'} alt=""/>
+                            <img src={'/img/barbers/Valerii.webp'} alt="Vitalii"/>
                         </S.BarberPhoto>
-                        <S.BarberName>Valerii</S.BarberName>
+                        <S.BarberName>Vitalii</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
                     </S.BarberInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Viktoria.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Viktoria.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Viktoria.webp'} alt=""/>
+                            <img src={'/img/barbers/Viktoria.webp'} alt="Victoriia"/>
                         </S.BarberPhoto>
-                        <S.BarberName>Viktoria</S.BarberName>
+                        <S.BarberName>Victoriia</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
                     </S.BarberInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
                             <source srcSet={'/img/barbers/Alina.webp'} type="image/webp"/>
                             <source srcSet={'/img/barbers/Alina.jpg'} type="image/jpg"/>
-                            <img src={'/img/barbers/Valeria.webp'} alt=""/>
+                            <img src={'/img/barbers/Valeria.webp'} alt="Alina"/>
                         </S.BarberPhoto>
                         <S.BarberName>Alina</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -52,7 +53,7 @@ const Barbers: FC = () => {
                 <S.JoinUsInfoWrapper>
                     <S.InfoTitle>{t('barbers_info_title')}</S.InfoTitle>
                     <S.InfoText>
-                        {t('barbers_info_text')} - <S.MailLink href={'mailto:unclevebarbershop@gmail.com'}>unclevebarbershop@gmail.com</S.MailLink>
+                        {t('barbers_info_text')} - <S.MailLink href={`mailto:${BARBERSHOP_MAIL_ADDRESS}`}>${BARBERSHOP_MAIL_ADDRESS}</S.MailLink>
                     </S.InfoText>
                 </S.JoinUsInfoWrapper>
             </PageWrapper>
