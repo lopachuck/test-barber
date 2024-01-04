d7ad42f061e0d5850717c5d75a36ac7c79512103
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-01-04 03:41:16 +0200

    code clear simpleModal.styles.ts

--- src/ui-kit/SimpleModal/simpleModal.styles.ts
+++ src/ui-kit/SimpleModal/simpleModal.styles.ts
@@ -1,14 +1,7 @@
 import styled from 'styled-components'
 
 export const Modal = styled.div`
-    //position: fixed;
-    //top: 0;
-    //left: 0;
-    //width: 100%;
-    //height: 100%;
-    background-color:
-            rgba(0, 0, 0, 0.75);
-    /* opacity: 0; */
+    background-color: rgba(0, 0, 0, 0.75);
     overflow: hidden;
     overflow-y: auto;
     transition: opacity 0.3s;
@@ -20,23 +13,13 @@ export const ContentWrapper = styled.div.attrs({
     position: absolute;
     top: 70px;
     left: -20px;
-    //left: -60px;
-    //left: 0;
     display: flex;
     justify-content: center;
     align-items: center;
-    //width: 100%;
-    //min-height: 100%;
 `
 export const Content = styled.div`
     position: relative;
-    //margin: 20px;
-    //width: 100%;
-    //max-width: 200px;
     border-radius: 20px;
-    //background-color: #ffffff;
-    //padding: 40px 20px 20px 20px;
     transform: translateY(-50px);
     transition: opacity 0.3s, transform 0.3s;
 `
-export const ContentWrapper2 = styled.div``
