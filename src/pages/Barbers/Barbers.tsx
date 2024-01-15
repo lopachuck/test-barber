ec96f9cedb3420d1d4efeb82ad86b2a8ca2d4d67
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-01-15 14:51:13 +0200

    Barbers.tsx webp | jpeg

--- src/pages/Barbers/Barbers.tsx
+++ src/pages/Barbers/Barbers.tsx
@@ -16,32 +16,32 @@ const Barbers: FC = () => {
                 <S.BarbersInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
-                            <source srcSet={'/img/barbers/barb1.webp, /img/barbers/barb1.jpg 2x'} type={"image/webp"}/>
-                            <img src={'/img/barbers/barb1.jpg'} loading={'lazy'} alt="barber1"/>
+                            <source srcSet={'/img/barbers/600webp/Valeria_600_600.webp 2x'} type={'image/webp'} />
+                            <img src={'/img/barbers/600jpeg/Valeria.jpeg'} loading={'lazy'} alt="Valeria" />
                         </S.BarberPhoto>
                         <S.BarberName>Valeria</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
                     </S.BarberInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
-                            <source srcSet={'/img/barbers/barb2.webp, /img/barbers/barb2.jpg 2x'} type="image/webp"/>
-                            <img src={'/img/barbers/barb2.jpg'} loading={'lazy'} alt="barber2"/>
+                            <source srcSet={'/img/barbers/600webp/Vitalii_600_600.webp 2x'} type="image/webp" />
+                            <img src={'/img/barbers/600jpeg/Vitalii.jpeg'} loading={'lazy'} alt="Vitalii" />
                         </S.BarberPhoto>
                         <S.BarberName>Vitalii</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
                     </S.BarberInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
-                            <source srcSet={'/img/barbers/barb3.webp, /img/barbers/barb3.jpg 2x'} type="image/webp"/>
-                            <img src={'/img/barbers/barb3.jpg'} loading={'lazy'} alt="barber3"/>
+                            <source srcSet={'/img/barbers/600webp/Viktoriia_600_600.webp 2x'} type="image/webp" />
+                            <img src={'/img/barbers/600jpeg/Viktoriia.jpeg'} loading={'lazy'} alt="Victoriia" />
                         </S.BarberPhoto>
                         <S.BarberName>Victoriia</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
                     </S.BarberInfo>
                     <S.BarberInfo>
                         <S.BarberPhoto>
-                            <source srcSet={'/img/barbers/barb4.webp, /img/barbers/barb4.jpg 2x'} type="image/webp"/>
-                            <img src={'/img/barbers/barb4.jpg'} loading={'lazy'} alt="barber4"/>
+                            <source srcSet={'/img/barbers/600webp/Alina_600_600.webp 2x'} type="image/webp" />
+                            <img src={'/img/barbers/600jpeg/Alina.jpeg'} loading={'lazy'} alt="Alina" />
                         </S.BarberPhoto>
                         <S.BarberName>Alina</S.BarberName>
                         <S.BarberPosition>{t('barbers_barber_position')}</S.BarberPosition>
@@ -50,7 +50,8 @@ const Barbers: FC = () => {
                 <S.JoinUsInfoWrapper>
                     <S.InfoTitle>{t('barbers_info_title')}</S.InfoTitle>
                     <S.InfoText>
-                        {t('barbers_info_text')} - <S.MailLink href={`mailto:${BARBERSHOP_MAIL_ADDRESS}`}>{BARBERSHOP_MAIL_ADDRESS}</S.MailLink>
+                        {t('barbers_info_text')} - <S.MailLink
+                        href={`mailto:${BARBERSHOP_MAIL_ADDRESS}`}>{BARBERSHOP_MAIL_ADDRESS}</S.MailLink>
                     </S.InfoText>
                 </S.JoinUsInfoWrapper>
             </PageWrapper>
@@ -58,4 +59,4 @@ const Barbers: FC = () => {
     )
 }
 
-export default Barbers;
+export default Barbers
