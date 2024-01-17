a6f6780274ff13ef918b8c1e222af3a47f706e77
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-01-17 15:50:19 +0200

    LogoLine.tsx refactorin adaptive for 4k

--- src/components/LogoLine/LogoLine.tsx
+++ src/components/LogoLine/LogoLine.tsx
@@ -1,48 +1,42 @@
-import {FC, MutableRefObject, useEffect, useLayoutEffect, useRef, useState} from "react";
-import {ReactComponent as IconLogo} from "../../assets/svg/banner/LogoLine.svg";
+import { FC, MutableRefObject, ReactComponentElement, useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { ReactComponent as IconLogo } from '../../assets/svg/banner/LogoLine.svg'
 import * as S from './logoLine.styles'
 import gsap from 'gsap'
-import {devices} from "../../devices";
+import { devices } from '../../devices'
 
 const LogoLine: FC = () => {
     const llw = useRef(null)
     const il = useRef() as MutableRefObject<SVGSVGElement>
     const [gap, setGap] = useState(S.GAP_DESKTOP)
-    
+
     useEffect(() => {
-        window.matchMedia(devices.sm).addEventListener( "change", (e) => {
-            if (e.matches) {
-                setGap(S.GAP_MOBILE)
-            } else {
-                setGap(S.GAP_DESKTOP)
-            }
+        window.matchMedia(devices.sm).addEventListener('change', (e) => {
+            setGap(e.matches ? S.GAP_MOBILE : S.GAP_DESKTOP)
         })
-    }, []);
-    
+    }, [])
+
     useLayoutEffect(() => {
         gsap.fromTo(
             llw.current,
-        {x: 0},
-        {x: -(il.current.clientWidth + gap)  + "px", duration: 5, repeat: -1, ease: "none" }
+            { x: 0 },
+            { x: -(il.current.clientWidth + gap) + 'px', duration: 5, repeat: -1, ease: 'none' },
         )
-    }, []);
+    }, [])
+    const logos: ReactComponentElement<any>[] = []
+    const COUNT_LOGOS: number = 13
+    for (let i = 0; i < COUNT_LOGOS; i++) {
+        logos.push(<IconLogo key={i} />)
+    }
     return (
         <>
             <S.LogoLineWrapper>
-                <div  ref={llw}>
-                    <IconLogo ref={il}/>
-                    <IconLogo/>
-                    <IconLogo/>
-                    <IconLogo/>
-                    <IconLogo/>
-                    <IconLogo/>
-                    <IconLogo/>
-                    <IconLogo/>
+                <div ref={llw}>
+                    <IconLogo ref={il} key={COUNT_LOGOS + 1} />
+                    {logos}
                 </div>
-                
             </S.LogoLineWrapper>
         </>
     )
 }
 
-export default LogoLine;
+export default LogoLine
